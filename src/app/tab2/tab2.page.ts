ecf8d365c7a269f460a8957479b81fe3d1feebea
Juan Jesus Mata Solis <jujemataso@ittepic.edu.mx>
2023-04-04 00:44:30 -0700

    PRODUCTS' LIST MOCKUP

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,14 @@
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { IonRatingStarsModule } from 'ion-rating-stars';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule, IonRatingStarsModule, NgOptimizedImage],
 })
 export class Tab2Page {
   constructor() {}
