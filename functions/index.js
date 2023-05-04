8b004b67b4a8284e40c449fc48e09cc22b532f28
Juan Jesus Mata Solis <jujemataso@ittepic.edu.mx>
2023-05-04 02:36:07 -0700

    FIREBASE FUNCTIONS

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+// The Cloud Functions for Firebase SDK to create Cloud Functions
+// and set up triggers.
 const functions = require("firebase-functions");
 
 // The Firebase Admin SDK to access Firestore.
@@ -7,24 +8,25 @@ admin.initializeApp();
 
 // Calculate the calification of a product every time a new opinion is added
 exports.calcCalification = functions.firestore
-  .document("/products/{documentId}/opinions/{opinionId}")
-  .onCreate((snap, context) => {
-    const opinion = snap.data();
-    const productId = context.params.documentId;
-    const productRef = admin.firestore().collection("products").doc(productId);
-    return admin.firestore().runTransaction(async (transaction) => {
-      const product = await transaction.get(productRef);
-      const productData = product.data();
-      const calification = productData.calification;
-      console.log(productId, productData);
-      console.log(opinion);
-      const numberOfOpinions = productData.numberOfOpinions;
-      const newCalification =
+    .document("/products/{documentId}/opinions/{opinionId}")
+    .onCreate((snap, context) => {
+      const opinion = snap.data();
+      const productId = context.params.documentId;
+      const productRef = admin.firestore()
+          .collection("products").doc(productId);
+      return admin.firestore().runTransaction(async (transaction) => {
+        const product = await transaction.get(productRef);
+        const productData = product.data();
+        const calification = productData.calification;
+        console.log(productId, productData);
+        console.log(opinion);
+        const numberOfOpinions = productData.numberOfOpinions;
+        const newCalification =
         (calification * numberOfOpinions + opinion.calification) /
         (numberOfOpinions + 1);
-      transaction.update(productRef, {
-        calification: newCalification,
-        numberOfOpinions: numberOfOpinions + 1,
+        transaction.update(productRef, {
+          calification: newCalification,
+          numberOfOpinions: numberOfOpinions + 1,
+        });
       });
     });
-  });
